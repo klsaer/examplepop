dffcfd4144f1c5e383c8c004b05dad56a9aadcb4
JuliaMuk <mukhina_ur@mail.ru>
2024-09-09 13:04:04 +0500

    create first route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/first', function () {
+    $a = 1;
+    $b = 2;
+    $c = $a + $b;
+    
+    return view('first',compact('c'));
+});
