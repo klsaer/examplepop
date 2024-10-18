67bb704dd441b31a50fa8752c27e1c3352789665
JuliaMuk <mukhina_ur@mail.ru>
2024-10-18 12:54:06 +0500

    fix breeze

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,15 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\StudentController;
+
+Route::get('/', [MainController::class, 'show'])->name('home');
+
+
+Route::get('/first', [MainController::class, 'first'])->name('first');
+
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -15,6 +24,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/students', [StudentController::class, 'index'])->name('students.index');
+    Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('students.destroy');
+
+    Route::post('/students', [StudentController::class, 'store'])->name('students.store');
+
+    Route::get('/students/{student}', [StudentController::class, 'show'])->name('students.show');
+
+    Route::put('/students/{student}', [StudentController::class, 'update'])->name('students.update');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
